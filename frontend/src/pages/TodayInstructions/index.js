7e6c2b1307ff39983483d0c4c0f961d29e30f2c3
GRFreire <gui.rugai.freire@gmail.com>
2020-07-05 18:51:53 -0300

    feat: add today instructions page

--- frontend/src/pages/TodayInstructions/index.js
+++ frontend/src/pages/TodayInstructions/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import { FiCheckCircle } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+import tempImage from '../../assets/images/tempImage.png'
+
+import { Container, SectionContainer, Sections, Section, SectionTitle, Steps, Step, Icon, StepDescription } from './styles';
+
+const sections = [
+  { id: 1, title: 'Antes da entrega', steps: [
+    'Deixe uma autorização na portaria do seu prédio para o entregador',
+    'Disponibilize uma máscara',
+    'Disponibilize álcool em gel',
+    'Utilize máscara',
+    'Higienize as mãos com álcool em gel'
+  ] },
+  { id: 2, title: 'Na entrega', steps: [
+    'Encontre o entregador',
+    'Tome distância de 1.5m',
+    'Receba o produto'
+  ] },
+  { id: 3, title: 'Após a entrega, como higienizar seu produto', steps: [
+    'Disponibilize uma máscara',
+    'Disponibilize álcool em gel',
+    'Utilize máscara',
+  ] },
+]
 
 function TodayInstructions() {
   return (
-    <>
-      <h1>Today Instructions</h1>
-    </>
+    <Container>
+      <Header
+        title="É Hoje"
+        imgSrc={tempImage}
+        description="Você receberá seu pedido pela FedEx"
+        instructions="Esses são os procedimentos que recomendamos que você siga"
+      />
+
+      <SectionContainer>
+        <Sections>
+          {sections.map((section) => (
+            <Section key={section.id}>
+              <SectionTitle>{section.title}</SectionTitle>
+              <Steps>
+                {section.steps.map((step) => (
+                  <Step key={step}>
+                    <Icon><FiCheckCircle size={24} color='#2CAFDC' /></Icon>
+                    <StepDescription>{step}</StepDescription>
+                  </Step>
+                ))}
+              </Steps>
+            </Section>
+          ))}
+        </Sections>
+      </SectionContainer>
+
+      <Footer />
+    </Container>
   );
 }
 
---
+++ frontend/src/pages/TodayInstructions/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const SectionContainer = styled.div`
+  margin: 30px;
+  margin-top: 60px;
+  margin-bottom: 160px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Sections = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Section = styled.div`
+  margin: 10px;
+  margin-top: 50px;
+`;
+
+export const SectionTitle = styled.h4`
+  font-size: 18pt;
+  font-weight: 500;
+`;
+
+export const Steps = styled.div`
+  margin: 15px;
+  margin-top: 25px;
+`;
+
+export const Step = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  margin: 10px;
+  margin-top: 35px;
+`;
+
+export const Icon = styled.div`
+  min-width: 24;
+  min-height: 24;
+`;
+
+export const StepDescription = styled.p`
+  margin-left: 15px;
+`;
+
